479977e27f9204249b7ded9ff2c5eafc3143a8c6
NareshGarva <garvanaresh1977@gmail.com>
2025-07-10 06:34:58 +0530

    add serach funcanility and see all skills in mentor card

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,9 @@ import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
+import Register from './pages/register/Register'
+import SearchResult from './pages/browse-mentor/components/SearchResult'
+import SearchSection from './pages/browse-mentor/components/SearchSection'
 
 
 
@@ -20,8 +23,13 @@ createRoot(document.getElementById('root')).render(
   <Route path='/' element={<Home/>}></Route>
   <Route path='/about-us' element={<About/>}></Route>
   <Route path='/contact' element={<Contact/>}></Route>
-  <Route path='/browse-mentor' element={<Browse_mentor/>}></Route>
+  <Route path='/browse-mentor' element={<Browse_mentor/>}>
+  <Route path='all-mentors' element={<SearchResult/>}></Route>
+  <Route path='search-mentors' element={<SearchSection/>}></Route>
+  </Route>
   <Route path='/login' element={<Login/>}></Route>
+  <Route path='/register' element={<Register/>}></Route>
+  <Route path="*" element={<h2>404 - Page Not Found 😢</h2>} />
   </Routes>
   <Footer/>
   </BrowserRouter>
--- frontend/src/pages/browse-mentor/Browse_mentor.jsx
+++ frontend/src/pages/browse-mentor/Browse_mentor.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Hero from './components/Hero'
 import SearchSection from './components/SearchSection'
 import SearchResult from './components/SearchResult'
+import { Mentors } from '../../sample/MentorCard'
 
 function Browse_mentor() {
+  const [filteredMentors, setFilteredMentors] = useState(Mentors);
   return (
     <>
    <Hero/>
-   <SearchSection/>
-   <SearchResult/>
+   <SearchSection setFilteredMentors={setFilteredMentors} />
+      <SearchResult filteredMentors={filteredMentors} />
     </>
   )
 }
