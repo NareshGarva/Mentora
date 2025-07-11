638d90a9ba57452118e3b4c64892d4d5f81e7bee
NareshGarva <garvanaresh1977@gmail.com>
2025-07-11 08:39:55 +0530

    Backend Dependencies installed and minor chnages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    proxy:"https://localhost:3000"
+  },
   plugins: [react(),
     tailwindcss(),
   ],
