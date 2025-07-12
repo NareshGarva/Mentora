be892087b8144316966ed6174045ae02a8781a64
Nishit Jain <nishit12345.jain@gmail.com>
2025-07-13 03:25:51 +0530

    Minor UI changes in ViewMentor.jsx

--- frontend/src/pages/LearnerModule/ViewMentor/ViewMentor.jsx
+++ frontend/src/pages/LearnerModule/ViewMentor/ViewMentor.jsx
@@ -7,62 +7,67 @@ import BookSessionBtn from "./components/BookSessionBtn";
 
 const ViewMentor = (props) => {
   return (
-    <>
-      <div className="left-section w-2/3">
-        {/* Mentor Overview Container */}
-        <div className="flex w-full p-9 gap-9 border-1 border-m-gray-400">
-          {/* Mentor Image Container */}
-          <div className="flex bg-m-gray-700 h-8 w-8"> </div>
-          {/* Mentor overview text container */}
-          <div className="flex flex-col gap-4">
-            <h2 className="text-4xl text-black">
-              {props.Mentor_name || "Mentor Name"}
-            </h2>
-            <h4 className="Mentor-tagline text-xl text-black">
-              {props.Mentor_tagline}
-            </h4>
-            <p className="location text-xl text-m-gray-600">
-              {props.Mentor_location || "Mentor's location"}
-            </p>
-            {/* ratings container */}
-            <div className="ratings-container flex gap-3">
-              <Star className="w-[1rem] h-[1rem]"></Star>
-              {props.mentor_rating || "0.0"}
-            </div>
-            {/* tags container*/}
-            <div className="tags-container w-full">
-              <SkillTag />
-              <SkillTag />
-              <SkillTag />
-              <SkillTag />
+    <div className="page-background bg-white h-screen w-screen">
+      <div className="All-sections flex gap-8 p-4">
+        <div className="left-section w-2/3 flex flex-col gap-8">
+          {/* Mentor Overview Container */}
+          <div className="flex w-full p-9 gap-9 border-1 border-m-gray-400 rounded-md">
+            {/* Mentor Image Container */}
+            <div className="flex bg-m-gray-700 h-8 w-8"> </div>
+            {/* Mentor overview text container */}
+            <div className="flex flex-col gap-4">
+              <h2 className="text-4xl text-black m-0">
+                {props.Mentor_name || "Mentor Name"}
+              </h2>
+              <h4 className="Mentor-tagline text-xl text-black">
+                {props.Mentor_tagline}
+              </h4>
+              <p className="location text-xl text-m-gray-600">
+                {props.Mentor_location || "Mentor's location"}
+              </p>
+              {/* ratings container */}
+              <div className="ratings-container flex gap-3 items-center">
+                <Star className="w-[1rem] h-[1rem]"></Star>
+                {props.mentor_rating || "0.0"}
+              </div>
+              {/* tags container*/}
+              <div className="tags-container flex gap-2 w-full">
+                <SkillTag />
+                <SkillTag />
+                <SkillTag />
+                <SkillTag />
+              </div>
             </div>
           </div>
-        </div>
 
-        {/* Bottom left container */}
-        <div className="flex w-full p-9 gap-9 border-1 border-m-gray-400">
-          <AboutMentor mentor_languages={["eng", "hin"]}/>
+          {/* Bottom left container */}
+          <div className="flex w-full p-9 gap-9 border-1 border-m-gray-400 rounded-md">
+            <AboutMentor mentor_languages={["eng", "hin"]} />
+          </div>
         </div>
-      </div>
-      <div className="right-section">
-        <div className="flex w-full p-9 gap-9 border-1 border-m-gray-400">
-          <div className="book-session-contents flex flex-col gap-4">
-            <Calendar className="w-[2rem] h-[2rem]" />
-            <h4 className="text-base font-bold">Session Duration</h4>
-            <div className="book-duration-options-container">
-              <BookDurationOption />
-              <BookDurationOption />
-              <BookDurationOption />
+        {/* right section*/}
+        <div className="right-section flex flex-col w-1/3 rounded-md">
+          <div className="flex w-full p-9 gap-9 border-1 border-m-gray-400">
+            <div className="book-session-contents flex flex-col gap-4 w-full">
+              <Calendar className="w-[2rem] h-[2rem]" />
+              <h4 className="text-base font-bold">Session Duration</h4>
+              <div className="book-duration-options-container flex flex-col w-full gap-4">
+                <BookDurationOption />
+                <BookDurationOption />
+                <BookDurationOption />
+              </div>
+              <div className="total-container flex justify-between">
+                <span className="text-base text-m-gray-600">Total: </span>
+                <span className="text-base text-black">
+                  {props.total || "0"}
+                </span>
+              </div>
+              <BookSessionBtn />
             </div>
-            <div className="total-container flex justify-between">
-              <span className="text-base text-m-gray-600">Total: </span>
-              <span className="text-base text-black">{props.total || "0"}</span>
-            </div>
-            <BookSessionBtn />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- frontend/src/pages/LearnerModule/ViewMentor/components/AboutMentor.jsx
+++ frontend/src/pages/LearnerModule/ViewMentor/components/AboutMentor.jsx
@@ -4,21 +4,21 @@ import SkillTag from './SkillTag'
 const AboutMentor = (props) => {
     // props.mentor_languages=["eng", "hin"]  
   return (
-    <div className='about-contents-wrapper flex gap-4 text-gray-600'>
+    <div className='about-contents-wrapper flex flex-col gap-6 text-gray-600'>
       <h3 className="text-2xl text-black font-bold">About</h3>
           <p>
             {props.mentor_about || "Mentor's description will appear here Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"}
           </p>
-          <div className="skills-and-language-container">
-            <div className="skills-container">
-                <h5 className='text-base font-bold text-black'>Skills</h5>
-                <SkillTag btnType="outlined" />
-                <SkillTag btnType="outlined" />
-                <SkillTag btnType="outlined" />
-                <SkillTag btnType="outlined" />
-                <SkillTag btnType="outlined" />
-                <SkillTag btnType="outlined" />
-                <SkillTag btnType="outlined" />
+          <div className="skills-and-language-container flex gap-4">
+            <div className="skills-container flex w-3/4 gap-2 flex-wrap">
+                <h5 className='text-base font-bold text-black block w-full'>Skills</h5>
+                <SkillTag type="outlined" />
+                <SkillTag type="outlined" />
+                <SkillTag type="outlined" />
+                <SkillTag type="outlined" />
+                <SkillTag type="outlined" />
+                <SkillTag type="outlined" />
+                <SkillTag type="outlined" />
             </div>
             <div className="language-container">
                 <h5 className='text-base font-bold'>Languages</h5>
