775a2993a5082b0b9a8ab1095b68a3606b2215be
Nishit Jain <nishit12345.jain@gmail.com>
2025-07-25 13:22:02 +0530

    Fixed formatting

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,32 +1,24 @@
-import React, {useEffect} from 'react'
-import HeroSection from './components/HeroSection'
-import WhyMentora from './components/WhyMentora'
-import FeaturedMentors from './components/FeaturedMentors'
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import HeroSection from "./components/HeroSection";
+import WhyMentora from "./components/WhyMentora";
+import FeaturedMentors from "./components/FeaturedMentors";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Home() {
   useEffect(() => {
     AOS.init({
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
     });
   }, []);
 
   return (
-   <main>
-    {/* Add hero section */}
-   <HeroSection/>
-
-
-
-  {/* Add Why chouse us section*/}
-<WhyMentora/>
-
-
-  {/* add Featured Mentors */}
-<FeaturedMentors/>
-   </main>
-  )
+    <main>
+      <HeroSection />
+      <WhyMentora />
+      <FeaturedMentors />
+    </main>
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/pages/ProfileModule/components/Overview.jsx
+++ frontend/src/pages/ProfileModule/components/Overview.jsx
@@ -8,109 +8,109 @@ function Overview() {
   const { user, isLoggedIn, isLoading, verifyUser } = useAuth();
 
   const receivedData = {};
-  if(user?.role === 'Mentee')
-  return (
-    <div className="flex flex-col gap-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
-        {/* Dashboard Cards*/}
-        <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-secondary-200 to bg-secondary-300 to-tertiary-100 w-full rounded-xl h-full flex items-center gap-6 ">
-          {/* Dashboard Card icon wrapper */}
-          <div className="p-3 rounded-xl bg-secondary-200">
-            <CalendarCheck className="text-secondary-600" />
+  if (user?.role === 'Mentee')
+    return (
+      <div className="flex flex-col gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
+          {/* Dashboard Cards*/}
+          <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-secondary-200 to bg-secondary-300 to-tertiary-100 w-full rounded-xl h-full flex items-center gap-6 ">
+            {/* Dashboard Card icon wrapper */}
+            <div className="p-3 rounded-xl bg-secondary-200">
+              <CalendarCheck className="text-secondary-600" />
+            </div>
+            {/* Dashboard Card text */}
+            <div>
+              <p className="text-gray-600">Sessions Attended</p>
+              <p className="text-3xl text-black font-bold">{10}</p>
+            </div>
           </div>
-          {/* Dashboard Card text */}
-          <div>
-            <p className="text-gray-600">Sessions Attended</p>
-            <p className="text-3xl text-black font-bold">{10}</p>
-          </div>
-        </div>
-        <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-yellow-200 to bg-yellow-300 to-white w-full rounded-xl h-full flex items-center gap-6 ">
-          {/* Dashboard Card icon wrapper */}
-          <div className="p-3 rounded-xl bg-yellow-200">
-            <CalendarClock className="text-yellow-600" />
-          </div>
+          <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-yellow-200 to bg-yellow-300 to-white w-full rounded-xl h-full flex items-center gap-6 ">
+            {/* Dashboard Card icon wrapper */}
+            <div className="p-3 rounded-xl bg-yellow-200">
+              <CalendarClock className="text-yellow-600" />
+            </div>
 
-          {/* Dashboard Card text */}
-          <div>
-            <p className="text-gray-600">Hours Learned</p>
-            <p className="text-3xl text-black font-bold">{0}</p>
+            {/* Dashboard Card text */}
+            <div>
+              <p className="text-gray-600">Hours Learned</p>
+              <p className="text-3xl text-black font-bold">{0}</p>
+            </div>
           </div>
-        </div>
-        <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-primary-200 to bg-primary-300 to-tertiary-100 w-full rounded-xl h-full flex items-center gap-6 ">
-          {/* Dashboard Card icon wrapper */}
-          <div className="p-3 rounded-xl bg-primary-200">
-            <TrendingUp className="text-primary-700" />
-          </div>
+          <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-primary-200 to bg-primary-300 to-tertiary-100 w-full rounded-xl h-full flex items-center gap-6 ">
+            {/* Dashboard Card icon wrapper */}
+            <div className="p-3 rounded-xl bg-primary-200">
+              <TrendingUp className="text-primary-700" />
+            </div>
 
-          {/* Dashboard Card text */}
-          <div>
-            <p className="text-gray-600">Goals Progress</p>
-            <p className="text-3xl text-black font-bold">67%</p>
+            {/* Dashboard Card text */}
+            <div>
+              <p className="text-gray-600">Goals Progress</p>
+              <p className="text-3xl text-black font-bold">67%</p>
+            </div>
           </div>
         </div>
+        <UpcomingSession />
       </div>
-      <UpcomingSession />
-    </div>
-  );
-  else{
+    );
+  else {
     return (
       <>
-          {/* Dashboard cards Container */}
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
-            {/* Dashboard Card 2 */}
-            <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-secondary-200 to bg-secondary-300 to-tertiary-300 w-full rounded-xl h-full flex items-center gap-6 ">
-              {/* Dashboard Card icon wrapper */}
-              <div className="p-3 w-fit h-fit rounded-2xl bg-secondary-200">
-                <IndianRupee className="w-[2rem] h-[2rem] stroke-secondary-700" />
-              </div>
-
-              {/* Dashboard Card text */}
-              <div className="dashboard-card-1-text flex flex-col h-auto">
-                <p className="dashboard-card-1-heading  text-base text-m-gray-700">
-                  Earnings this month
-                </p>
-                <p className="dashboard-card-1-content text-3xl text-black font-bold">
-                  {receivedData.sessionsAttended || 0}
-                </p>
-              </div>
+        {/* Dashboard cards Container */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
+          {/* Dashboard Card 2 */}
+          <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-secondary-200 to bg-secondary-300 to-tertiary-300 w-full rounded-xl h-full flex items-center gap-6 ">
+            {/* Dashboard Card icon wrapper */}
+            <div className="p-3 w-fit h-fit rounded-2xl bg-secondary-200">
+              <IndianRupee className="w-[2rem] h-[2rem] stroke-secondary-700" />
             </div>
 
-            <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-primary-200 to bg-primary-300 to-tertiary-100 w-full rounded-xl h-full flex items-center gap-8  sm:p-3">
-              {/* Dashboard Card icon wrapper */}
-              <div className="p-3 w-fit h-fit rounded-2xl bg-primary-100">
-                <Users className="w-[2rem] h-[2rem] stroke-primary-600" />
-              </div>
-
-              {/* Dashboard Card text */}
-              <div className="dashboard-card-1-text flex flex-col h-auto">
-                <p className="dashboard-card-1-heading  text-base text-m-gray-700">
-                  Sessions
-                </p>
-                <p className="dashboard-card-1-content text-3xl text-black font-bold">
-                  {receivedData.sessions || 0}
-                </p>
-              </div>
-            </div>
-
-            <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-orange-200 to bg-orange-300 to-white w-full rounded-xl h-full flex items-center gap-8  ">
-              {/* Dashboard Card icon wrapper */}
-              <div className="p-3 w-fit h-fit rounded-2xl bg-orange-100">
-                <Star className="w-[2rem] h-[2rem] stroke-orange-400" />
-              </div>
-
-              {/* Dashboard Card text */}
-              <div className="dashboard-card-1-text flex flex-col h-auto">
-                <p className="dashboard-card-1-heading  text-base text-m-gray-700">
-                  Avg. Rating
-                </p>
-                <p className="dashboard-card-1-content text-3xl text-black font-bold">
-                  {receivedData.average_rating || 0}
-                </p>
-              </div>
+            {/* Dashboard Card text */}
+            <div className="dashboard-card-1-text flex flex-col h-auto">
+              <p className="dashboard-card-1-heading  text-base text-m-gray-700">
+                Earnings this month
+              </p>
+              <p className="dashboard-card-1-content text-3xl text-black font-bold">
+                {receivedData.sessionsAttended || 0}
+              </p>
             </div>
           </div>
-<UpcomingSession />
-    </>
+
+          <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-primary-200 to bg-primary-300 to-tertiary-100 w-full rounded-xl h-full flex items-center gap-8  sm:p-3">
+            {/* Dashboard Card icon wrapper */}
+            <div className="p-3 w-fit h-fit rounded-2xl bg-primary-100">
+              <Users className="w-[2rem] h-[2rem] stroke-primary-600" />
+            </div>
+
+            {/* Dashboard Card text */}
+            <div className="dashboard-card-1-text flex flex-col h-auto">
+              <p className="dashboard-card-1-heading  text-base text-m-gray-700">
+                Sessions
+              </p>
+              <p className="dashboard-card-1-content text-3xl text-black font-bold">
+                {receivedData.sessions || 0}
+              </p>
+            </div>
+          </div>
+
+          <div className="dashboard-card shadow-sm p-6 bg-gradient-to-tl from-orange-200 to bg-orange-300 to-white w-full rounded-xl h-full flex items-center gap-8  ">
+            {/* Dashboard Card icon wrapper */}
+            <div className="p-3 w-fit h-fit rounded-2xl bg-orange-100">
+              <Star className="w-[2rem] h-[2rem] stroke-orange-400" />
+            </div>
+
+            {/* Dashboard Card text */}
+            <div className="dashboard-card-1-text flex flex-col h-auto">
+              <p className="dashboard-card-1-heading  text-base text-m-gray-700">
+                Avg. Rating
+              </p>
+              <p className="dashboard-card-1-content text-3xl text-black font-bold">
+                {receivedData.average_rating || 0.0}
+              </p>
+            </div>
+          </div>
+        </div>
+        <UpcomingSession />
+      </>
     )
   }
 }
